a6ae8102a2c14cc321f28cffc73605f57aabee15
VukV <vuk.vukovic.2000@gmail.com>
2023-01-02 14:29:45 +0100

    bug fixes

--- src/main/java/com/raf/cloudproviderbackend/repository/MachineRepository.java
+++ src/main/java/com/raf/cloudproviderbackend/repository/MachineRepository.java
@@ -14,7 +14,7 @@ public interface MachineRepository extends JpaRepository<Machine, Long> {
 
     @Query("select m from Machine m where m.createdBy.email = :email " +
             "and (:machineName is null or m.name like %:machineName%) " +
-            "and (:statusList is null  or m.machineStatus in :statusList) " +
+            "and ((:statusList) is null  or m.machineStatus in (:statusList)) " +
             "and (cast(:dateFrom as date) is null or m.createdDate >= :dateFrom) " +
             "and (cast(:dateTo as date) is null or m.createdDate <= :dateTo)")
     List<Machine> findAllMachines(String email, String machineName, List<MachineStatusEnum> statusList, Date dateFrom, Date dateTo);
